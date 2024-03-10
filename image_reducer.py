9a2c656b7839f1a9521614f89480d56bc305d6f1
Hansespinosa2 <andresespinosa@ufl.edu>
2024-03-10 09:31:45 -0500

    Implement optional clustering for colors

--- image_reducer.py
+++ image_reducer.py
@@ -2,6 +2,7 @@ import os
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.cluster import KMeans
 
 
 def get_file_paths(folder:str):
@@ -9,33 +10,40 @@ def get_file_paths(folder:str):
         the_file_paths = [os.path.join(root,path) for path in file_paths]
     return the_file_paths
 
-def crop_and_cut(file_path,smallest_crop):
-    an_image = plt.imread(file_path)
-    length = an_image.shape[0]
-    width = an_image.shape[1]
-    if length < smallest_crop or width < smallest_crop:
-        print(f'{file_path} is smaller than the crop size, skipping')
-        exit
-    start_l = (length - smallest_crop)//2
-    start_w = (width-smallest_crop)//2
-    an_image = an_image[start_l:smallest_crop+start_l,start_w:smallest_crop+start_w,:3]
-    return an_image
 
-def make_images(file_path_list:list,smallest_crop):
-    return [crop_and_cut(path,smallest_crop) for path in file_path_list]
+def segment_colors(an_image,n_clusters):
+    original_shape = an_image.shape
+    X = np.array(an_image).reshape(-1, 4)
+    kmeans = KMeans(n_clusters=n_clusters, random_state=42).fit(X)
+    segmented_img = kmeans.cluster_centers_[kmeans.labels_]
+    return segmented_img.reshape(original_shape)
+
+def crop_and_cut(file_path,smallest_crop,n_clusters):
+    an_image = plt.imread(file_path)
+    if n_clusters is not None:        
+        an_image = segment_colors(an_image,n_clusters)
+
+    an_image = tf.keras.layers.Resizing(smallest_crop,smallest_crop,crop_to_aspect_ratio=True)(an_image)
+    return np.array(an_image)
+
+def make_images(file_path_list:list,smallest_crop,color_restriction):
+    return [crop_and_cut(path,smallest_crop,color_restriction) for path in file_path_list]
 
 
 def display_and_save_images(output,path):
     for i,img in enumerate(output):
         plt.imshow(np.array(img))
         plt.axis('off')
-        plt.savefig(path + '/' + f'{i}.png')
+        plt.savefig(path + '/' + f'{i}.png',transparent=True)
 
     
 
-def reduce_image_folder(folder:str, smallest_crop:int=320, pool:int=4):
+
+
+
+def reduce_image_folder(folder:str, smallest_crop:int=320, pool:int=4,color_restriction=10):
     the_file_paths = get_file_paths(folder)
-    the_images = make_images(the_file_paths,smallest_crop)
+    the_images = make_images(the_file_paths,smallest_crop,color_restriction)
     the_images = tf.keras.layers.CenterCrop(height=smallest_crop,width=smallest_crop)(the_images)
 
     max_pool = tf.keras.layers.MaxPool2D(pool_size=pool)
